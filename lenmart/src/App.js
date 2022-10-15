472ecd4523a31e105e87ff63aced717ebcf444dd
lensonfernandes <lenson.fernand@gmail.com>
2022-10-15 15:45:54 +0530

    NavBar Structure added

--- lenmart/src/App.js
+++ lenmart/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/NavBar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      This is Test Message
+      {/* This is Test Message */}
+      <Navbar />
     </div>
   );
 }
